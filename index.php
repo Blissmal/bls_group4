62cd88d7270dc648a5b701615b9d44c1001eb6aa
Blissmal <121401934+Blissmal@users.noreply.github.com>
2024-03-11 20:19:33 +0300

    main php file

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+$routes = [
+    "" => "views/home.php",
+    "/" => "views/home.php",
+    "/buy" => "views/buy.php",
+    "/home" => "views/home.php",
+    "/hire" => "views/hire.php",
+    "/index" => "views/home.php",
+    "/login" => "views/login.php",
+    "/about" => "views/about.php",
+    "/search" => "views/search.php",
+    "/index.php" => "views/home.php",
+    "/contact" => "views/contact.php",
+    "/logout" => "php/config/logout.php"
+    
+];
+
+$request_url = $_SERVER["REQUEST_URI"];
+$request_url = strtok($request_url, "?");
+$request_url = rtrim($request_url, "/");
+
+
+if (array_key_exists($request_url, $routes)) {
+    include_once $routes[$request_url];
+} else {
+    include_once "views/404.php";
+}
+?>
